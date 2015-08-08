4ad6decc4d6c5ed5069a200ba65dd821873f989f
simpletrain <zsy498761554@live.com>
2015-08-08 12:36:18 +0800

    add modal

--- js/main.js
+++ js/main.js
@@ -1,42 +1,45 @@
 function checkout() {
-  var information = new Information($('#class-name').val(),
-                                    $('#id-number').val(),
-                                    $('#student-name').val());
-  if(!information.check()){
-    return false;
+  $(document).ready(function(){
+
+    var information = new Information($('#class-name').val(),
+        $('#id-number').val(),
+        $('#student-name').val());
+    if(!information.check()){
+      return false;
+    }
+
+    var score = new Score();
+
+    var fibInputs = [['one-one'],['one-two','one-three','one-four']];
+    var fibAnswer = [['统一建模语言'],['1','2','3']];
+    var fillInBlanks = new FillInBlanks(fibInputs,fibAnswer);
+    fillInBlanks.getInputsValues();
+    fillInBlanks.checkAnswer(score);
+
+    var singleChooseInputs = ['two-one','two-two'];
+    var singleChooseAnswer = ['A','A'];
+    var singleChoose = new SingleChoose(singleChooseInputs,singleChooseAnswer);
+    singleChoose.getInputsValues();
+    singleChoose.checkAnswer(score);
+
+    var multiChooseInputs = ['three-one','three-two'];
+    var multiChooseAnswer = ['ABD','ABC'];
+    var multiChoose = new MultiChoose(multiChooseInputs,multiChooseAnswer);
+    multiChoose.getInputsValues();
+    multiChoose.checkAnswer(score);
+
+    var judgeInputs = ['four-one','four-two'];
+    var judgeAnswer = ['false','true'];
+    var judge = new Judge(judgeInputs,judgeAnswer);
+    judge.getInputsValues();
+    judge.checkAnswer(score);
+
+    var subjectiveInputs = ['five-one'];
+    var SubjectiveAnswer = ['我不会'];
+    var subjective = new Subjective(subjectiveInputs,SubjectiveAnswer);
+    subjective.getInputsValues();
+    subjective.checkAnswer(score);
+
+    score.showScore();
+  });
   }
-
-  var score = new Score();
-
-  var fibInputs = [['one-one'],['one-two','one-three','one-four']];
-  var fibAnswer = [['统一建模语言'],['1','2','3']];
-  var fillInBlanks = new FillInBlanks(fibInputs,fibAnswer);
-  fillInBlanks.getInputsValues();
-  fillInBlanks.checkAnswer(score);
-
-  var singleChooseInputs = ['two-one','two-two'];
-  var singleChooseAnswer = ['A','A'];
-  var singleChoose = new SingleChoose(singleChooseInputs,singleChooseAnswer);
-  singleChoose.getInputsValues();
-  singleChoose.checkAnswer(score);
-
-  var multiChooseInputs = ['three-one','three-two'];
-  var multiChooseAnswer = ['ABD','ABC'];
-  var multiChoose = new MultiChoose(multiChooseInputs,multiChooseAnswer);
-  multiChoose.getInputsValues();
-  multiChoose.checkAnswer(score);
-
-  var judgeInputs = ['four-one','four-two'];
-  var judgeAnswer = ['false','true'];
-  var judge = new Judge(judgeInputs,judgeAnswer);
-  judge.getInputsValues();
-  judge.checkAnswer(score);
-
-  var subjectiveInputs = ['five-one'];
-  var SubjectiveAnswer = ['我不会'];
-  var subjective = new Subjective(subjectiveInputs,SubjectiveAnswer);
-  subjective.getInputsValues();
-  subjective.checkAnswer(score);
-
-  score.showScore();
-}
--- js/model/information.js
+++ js/model/information.js
@@ -6,17 +6,23 @@ function Information(className,idNumber,studentName){
 
 Information.prototype.check = function(){
   if(!this.className){
-    alert('请填写班级！');
+    $('#class-modal').modal('show');
+    $('#form-class').addClass('has-error');
+    $('#class-name').focus();
     return false;
   }
 
   if(!this.idNumber){
-    alert('请填写学号！');
+    $('#id-modal').modal('show');
+    $('#form-id').addClass('has-error');
+    $('#id-number').focus();
     return false;
   }
 
   if(!this.studentName){
-    alert('请填写姓名！');
+    $('#name-modal').modal('show');
+    $('#form-name').addClass('has-error');
+    $('#student-name').focus();
     return false;
   }
   return true;
