ae8a26d2297f062f9ac82e4c9e892d45b67ec6e3
simpletrain <zsy498761554@live.com>
2015-08-09 22:31:55 +0800

    use express-generator

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
 var app = express();
 
-app.set('view engine','ejs');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/',function(res,req){
-    req.send('Hi');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.use('/', routes);
+
+app.get('/', function(request, response) {
+  response.render('index');
 });
 
-app.get('/ejs',function(res,req){
-    req.render('default',{name:['Mike','Jack','Lucy']});
+
+
+
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.listen(3000);
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
