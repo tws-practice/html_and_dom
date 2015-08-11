924980484c13fb56cc73c5eedcc7d860058726ec
simpletrain <zsy498761554@live.com>
2015-08-11 21:12:15 +0800

    clean up all stuff&ready to refactor

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Score = require('../controller/score.js');
+var dataFeedBack = require('../controller/data-feed-back.js');
+var jsonFile = require('../data.json');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -9,21 +11,30 @@ router.get('/', function (req, res) {
             score: '',
             className: '',
             idNumber: '',
-            studentName: ''
+            studentName: '',
+            fill_in_blanks_detail:''
         });
 });
 
 router.post('/', function (req, res) {
     var data = req.body;
-    console.log(data);
+
     var score = new Score(data);
     score.calculate();
+
+    var feedBackJson = dataFeedBack(data,jsonFile);
+
     res.render('index',
         {
             score: score.score,
             className: data.className,
             idNumber: data.idNumber,
-            studentName: data.studentName
+            studentName: data.studentName,
+            fill_in_blanks_detail:feedBackJson.questions.fill_in_blanks.detail
+            //single_choose_detail:feedBackJson.questions.single_choose.detail,
+            //multi_choose_detail:feedBackJson.questions.multi_choose.detail,
+            //judge_detail:feedBackJson.questions.judge.detail,
+            //subjective_detail:feedBackJson.questions.subjective.detail
         });
 });
 
