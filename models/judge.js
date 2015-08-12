6e2fb617b3f5e9b59b57b1d815dbffdc2aee859e
simpletrain <zsy498761554@live.com>
2015-08-12 16:20:12 +0800

    after submit can return right view

--- models/judge.js
+++ models/judge.js
@@ -1,7 +1,7 @@
 var Topic = require('./topic.js');
 
 function JudgeTopic(question,options,answer,score,input){
-    Topic.call(this,'Judge',question,options,answer,score,input);
+    Topic.call(this,'judge',question,options,answer,score,input);
 }
 
 JudgeTopic.prototype = Object.create(Topic.prototype);
