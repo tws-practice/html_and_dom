19587c0625a48a87dd8ee5e9234a8e597687451d
simpletrain <zsy498761554@live.com>
2015-08-12 13:15:50 +0800

    can get right view

--- controller/first-call.js
+++ controller/first-call.js
@@ -2,10 +2,27 @@ var testPaper = require('../config/test-paper.json');
 var topicFactory = require('../models/factory.js');
 
 function firstCall(req, res) {
-    var allTopic = [];
-    
-    for(var i = 0;i < testPaper.paper.length;i++){
-        allTopic.push(topicFactory(testPaper.paper[i]));
+    var allTopic = {
+        fill_in_blanks:[],
+        single_choose:[],
+        multi_choose:[],
+        judge:[],
+        subjective:[]
+    };
+
+    for (var i = 0; i < testPaper.paper.length; i++) {
+        if (testPaper.paper[i].type === 'fill_in_blanks') {
+            allTopic.fill_in_blanks.push(topicFactory(testPaper.paper[i]));
+        } else if (testPaper.paper[i].type === 'single_choose') {
+            allTopic.single_choose.push(topicFactory(testPaper.paper[i]));
+        } else if (testPaper.paper[i].type === 'multi_choose') {
+            allTopic.multi_choose.push(topicFactory(testPaper.paper[i]));
+        } else if (testPaper.paper[i].type === 'judge') {
+            allTopic.judge.push(topicFactory(testPaper.paper[i]));
+        } else if (testPaper.paper[i].type === 'subjective') {
+            allTopic.subjective.push(topicFactory(testPaper.paper[i]));
+        }
+
     }
     console.log(allTopic);
 
@@ -14,7 +31,8 @@ function firstCall(req, res) {
             score: '',
             className: '',
             idNumber: '',
-            studentName: ''
+            studentName: '',
+            allTopic:allTopic
         });
 }
 
